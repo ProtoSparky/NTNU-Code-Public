8cae40f1469bd91dcabbc9a9b4253a2677c99e2a
Sparky <81528500+ProtoSparky@users.noreply.github.com>
2025-09-22 14:05:36 +0200

    Oblig 1 rewritten

--- PROG1001/09.09.2025-Oblig_1/oblig1.c
+++ PROG1001/09.09.2025-Oblig_1/oblig1.c
@@ -1,57 +1,71 @@
 /**
- *   Obligatorisk oppgave nummer 1.
- *
- *   Samler inn navn, sidetall, og sjanger på 2 ulike bøker
- *
  *   @file     oblig1.c
  *   @author   Kristupas Kaupas
+ *   @date     2025-09-22
+ *   @brief    Obligatorisk oppgave nummer 1
+ *   @details  Samler inn navn, sidetall, og sjanger på 2 ulike bøker
  */
 
-#include <stdio.h>
-#define STRLEN 512                  //512 char så de fleste titler får plass
-char bok1[STRLEN], bok2[STRLEN];    //Setter opp variabler for navn på bøker
-int sider[2]; char sjanger[2]={};   //-||- men for sjanger og sider
+#include <stdio.h>                  // printf, scanf, gets
+#define STRLEN 512                  ///< Max input char lengde for bøker
 
-//lager en funksjon som spør bruker om bok info
-//blir brukt flere ganger
-//scanf() erstatter gets() pga den er ikke lenger med i nyere C versjoner!!
+// GLOBALE VARIABLER
+char bok1[STRLEN], bok2[STRLEN];    ///< Bok navn
+int sider[2];                       ///< Sidetall ints for begge bøkene
+char sjanger[2]={};                 ///< Sjanger array for begge bøkene (B,F,T)
+
+// DEKLARASJON AV FUNKSJONER
+void getBookInfo(int bookId);
+
+
+
+/**
+ * @brief Startpunktet for programmet.
+ * @details Henter informasjon om 2 bøker via getBookInfo, og viser dem pent.
+ * @return returnerer int 0 etter den var kjørt
+ */
+int main(){
+    printf("************************************************\n");
+    getBookInfo(0);     //Henter bok data fra bruker for bok 1
+    printf("************************************************\n");
+    getBookInfo(1);     //Henter bok data fra bruker for bok 2
+    printf("************************************************\n");
+
+    //Skriver ut data som på eksempel
+                                                    //Bok 1
+    printf("Navn: %s \n",bok1);
+    printf("Sjanger: %c \n\n", sjanger[0]);
+                                                    //Bok 2
+    printf("Navn: %s \n", bok2);
+    printf("Sjanger: %c \n\n", sjanger[1]);        
+
+    printf("Bøker lest: 2 \n");
+    printf("Sider lest totalt: %i \n", sider[0] + sider[1]); //summerer sidetall
+
+    printf("************************************************\n");
+    return 0;
+}
+
+
+/**
+*   @brief Spør bruker om info for bøker.
+*   @param bookId input integer for kontroll av bok navn array som brukes.(0,1)
+*   @note scanf() erstatter gets() i hengold med nyere C versjoner
+*/
 void getBookInfo(int bookId){
-    if(bookId == 0){        //bruker if for å lagre data i separate variabler
+    if(bookId == 0){        //Bruker if for å lagre data i separate variabler
         printf("Navn på bok %i: \n",bookId + 1); 
-        scanf("%511[^\n]", &bok1);  //leser input til newline. maks 511 char
+        scanf("%511[^\n]", &bok1);  //Leser input til newline. maks 511 char
     }else{
         printf("Navn på bok %i: \n",bookId + 1); 
         scanf("%511[^\n]", &bok2);
     }   
-    getchar();
-
+    getchar(); //Tømmer \n fra buffer etter if seksjonen
+                                                    
     printf("Antall sider på bok %i: \n",bookId + 1);
-    scanf(" %d",&sider[bookId]); getchar();
+    scanf(" %d",&sider[bookId]); getchar();     //Henter sider på bok n
 
     printf("Sjanger på bok (B - Biografi, F - Fiksjon, T - Thriller): \n");
-    scanf(" %c",&sjanger[bookId]); getchar();    
+    scanf(" %c",&sjanger[bookId]); getchar();    //Henter sjanger char
     
-}
-
-// ***************************************************************************
-// ***************************************************************************
-
-int main(){
-    printf("************************************************\n");
-    getBookInfo(0);     //Kjører for bok 1
-    printf("************************************************\n");
-    getBookInfo(1);     //Kjører for bok 2
-    printf("************************************************\n");
-    //printer ut data som vist på eksempel
-    printf("Navn: %s \n",bok1);
-    printf("Sjanger: %c \n\n", sjanger[0]);
-
-    printf("Navn: %s \n", bok2);
-    printf("Sjanger: %c \n\n", sjanger[1]);
-
-    printf("Bøker lest: 2 \n");
-    printf("Sider lest totalt: %i \n", sider[0] + sider[1]); 
-
-    printf("************************************************\n");
-    return 0;
 }
