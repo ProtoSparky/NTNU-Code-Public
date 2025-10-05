65181e97b6cfa214d30949e9a778c654137700c6
Sparky <81528500+ProtoSparky@users.noreply.github.com>
2025-10-05 19:15:08 +0200

    starting on oblig2

---
+++ PROG1001/05.10.2025-Oblig_2/oblig2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+const int MAXBANER = 18; ///< Maks antall baner.
+const int STRLEN = 100; ///< Maks tekstlengde.
+
+int main(){
+    char baneBeskrivelse[MAXBANER][STRLEN]; // Beskrivelse av banene.
+    int baneLengde[MAXBANER]; // Banenes lengde (i meter).
+    int banePar[MAXBANER]; // Forventet antall kast (par)
+    // for hver enkelt bane.
+    bool baneOB[MAXBANER]; // Har banene OB eller ei
+    // (google evt. hva OB er).
+    int antallBaner; // Antall baner pr.nå
+
+}
