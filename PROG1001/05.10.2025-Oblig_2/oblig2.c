5cf7827c1719ac608176df9685feeb3ded43882d
Sparky <81528500+ProtoSparky@users.noreply.github.com>
2025-10-06 10:20:43 +0200

    got method down

--- PROG1001/05.10.2025-Oblig_2/oblig2.c
+++ PROG1001/05.10.2025-Oblig_2/oblig2.c
@@ -4,6 +4,8 @@
 const int MAXBANER = 18; ///< Maks antall baner.
 const int STRLEN = 100; ///< Maks tekstlengde.
 
+int menu();
+
 int main(){
     char baneBeskrivelse[MAXBANER][STRLEN]; // Beskrivelse av banene.
     int baneLengde[MAXBANER]; // Banenes lengde (i meter).
@@ -13,4 +15,30 @@ int main(){
     // (google evt. hva OB er).
     int antallBaner; // Antall baner pr.nå
 
+    menu(); 
+    return 0;
+}
+
+
+int menu(){
+    char userInput ;
+    printf("Menyvalg:");
+    printf("\n    L - Legge til bane");
+    printf("\n    S - Se alle baner");
+    printf("\n    Q - Asvlutt");
+    printf("\nValg:");
+
+    
+    scanf("%c", &userInput);
+
+    switch(userInput):
+        case "L":
+        printf("Bruker L");
+        break;
+        case "S":
+        printf("Bruker S");
+        
+
+
+    return 0; 
 }
