037eb29cce72f28eb04aad456be20be8259f1c0f
Sparky <kristupmaker@gmail.com>
2026-01-06 12:20:09 +0100

    prog

---
+++ PROG1003/06.01.2026/06.01.2026.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+using namespace std; //Dont need std::cout /std::cin
+int main()
+{
+
+    /*
+    int totalSeconds = 125, minutes, seconds;
+    minutes = totalSeconds / 60;
+    seconds = totalSeconds % 60;
+    cout << totalSeconds << " seconds is equivalent to " << minutes << " minutes and " << seconds << " seconds \n"; 
+    
+    return 0;*/ 
+
+    /*
+
+    int length, width, area;
+    
+    cout << "Wnat is the length of the rectangle? "; 
+    cin >> length;
+
+    cout << " \n" << length;
+    */
+    /*
+    int value, value2; 
+    value = 50;
+    value2 = 59;
+    float value3;
+    value3 = static_cast <float> (value) / (value2); 
+    */
+
+    /*
+    * requires included iomanip
+    cout << setw(6) << 10 << setw(6) << 900 << setw(6);
+    */
+
+    char tegn;
+    char string[100];
+    
+}
